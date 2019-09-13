1ab5a43f55118b01c432978962c09000884dcd71
Sam Walker <jack.nasal@gmail.com>
2019-09-13 11:20:20 -0400

    removed brackets from "array" of genres

--- seeds/seed.valet_tables.sql
+++ seeds/seed.valet_tables.sql
@@ -16,14 +16,14 @@ VALUES
 
 INSERT INTO games (title, short_description, long_description, genres, img, rules, tips, rating)
 VALUES
-  ('games 1', 'this is game 1', 'the game that this is is 1', '[a,b,c]', 'img.jpg', 'rules.pdf', 'win', 5),
-  ('games 2', 'this is game 2', 'the game that this is is 2', '[a,d,e]', 'img.jpg', 'rules.pdf', 'win', 4),
-  ('games 3', 'this is game 3', 'the game that this is is 3', '[d,e,f]', 'img.jpg', 'rules.pdf', 'win', 3),
-  ('games 4', 'this is game 4', 'the game that this is is 4', '[h,i,g]', 'img.jpg', 'rules.pdf', 'win', 4),
-  ('games 5', 'this is game 5', 'the game that this is is 5', '[g,b,e]', 'img.jpg', 'rules.pdf', 'win', 5),
-  ('games 6', 'this is game 6', 'the game that this is is 6', '[g]', 'img.jpg', 'rules.pdf', 'win', 2),
-  ('games 7', 'this is game 7', 'the game that this is is 7', '[a,b,d]', 'img.jpg', 'rules.pdf', 'win', 3),
-  ('games 8', 'this is game 8', 'the game that this is is 8', '[i,f,g]', 'img.jpg', 'rules.pdf', 'win', 1);
+  ('games 1', 'this is game 1', 'the game that this is is 1', 'a,b,c', 'img.jpg', 'rules.pdf', 'win', 5),
+  ('games 2', 'this is game 2', 'the game that this is is 2', 'a,d,e', 'img.jpg', 'rules.pdf', 'win', 4),
+  ('games 3', 'this is game 3', 'the game that this is is 3', 'd,e,f', 'img.jpg', 'rules.pdf', 'win', 3),
+  ('games 4', 'this is game 4', 'the game that this is is 4', 'h,i,g', 'img.jpg', 'rules.pdf', 'win', 4),
+  ('games 5', 'this is game 5', 'the game that this is is 5', 'g,b,e', 'img.jpg', 'rules.pdf', 'win', 5),
+  ('games 6', 'this is game 6', 'the game that this is is 6', 'g', 'img.jpg', 'rules.pdf', 'win', 2),
+  ('games 7', 'this is game 7', 'the game that this is is 7', 'a,b,d', 'img.jpg', 'rules.pdf', 'win', 3),
+  ('games 8', 'this is game 8', 'the game that this is is 8', 'i,f,g', 'img.jpg', 'rules.pdf', 'win', 1);
 
 INSERT INTO user_games_junction (user_id, game_id, deleted)
 VALUES
